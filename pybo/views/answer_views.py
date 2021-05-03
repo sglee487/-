8c29b936d3f5932d228d4286fec43f56bccbc89f
gun <sglee487@gmail.com>
2021-05-04 01:18:41 +0900

    ~p232

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 # Create your views here.
 from django.utils import timezone
 
@@ -26,7 +26,7 @@ def answer_create(request, question_id):
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pybo:detail', question_id=question.id)
+            return redirect(f"{resolve_url('pybo:detail', question_id=question.id)}#answer_{answer.id}")
     else:
         form = AnswerForm()
     context = {'question': question, 'form': form}
@@ -49,7 +49,7 @@ def answer_modify(request, answer_id):
             answer.author = request.user
             answer.modify_date = timezone.now()
             answer.save()
-            return redirect('pybo:detail', question_id=answer.question.id)
+            return redirect(f"{resolve_url('pybo:detail', question_id=answer.question.id)}#answer_{answer.id}")
     else:
         form = AnswerForm(instance=answer)
     context = {'answer': answer, 'form': form}
